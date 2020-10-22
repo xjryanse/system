ab787fb14e6f71ea0dafa343b66b07106510c444
xieJialun <36151536+xjryanse@users.noreply.github.com>
2020-10-22 14:24:02 +0800

    hasCompany

--- src/logic/CompanyLogic.php
+++ src/logic/CompanyLogic.php
@@ -3,6 +3,8 @@ namespace xjryanse\system\logic;
 
 use xjryanse\logic\Arrays;
 use xjryanse\system\service\SystemCompanyService;
+use think\facade\Request;
+use Exception;
 /**
  * 公司逻辑
  */
@@ -20,6 +22,26 @@ class CompanyLogic
         }
         
         return SystemCompanyService::getByKey( $key );
+    }
+    /**
+     * 
+     * @return type
+     * @throws Exception
+     */
+    public static function hasCompany()
+    {
+        //从checkCompany搬过来-开始
+        $comKey     = Request::param('comKey','') ? : session('scopeCompanyKey');
+        if( !$comKey ){
+            throw new Exception('请求入口错误');
+        }
+        $info = SystemCompanyService::getByKey( $comKey );
+        if( !$info){
+            throw new Exception('未找到company信息');
+        }
+        session('scopeCompanyKey',$comKey);
+        session('scopeCompanyId',$info['id']);        
+        return $info;
     }
 
 
