2bf0c4b850f5e1ed4e9103b1f6affcb8ad7b3f4c
xieJialun <36151536+xjryanse@users.noreply.github.com>
2020-11-19 10:18:06 +0800

    配置模型完善

--- src/model/SystemConfigs.php
+++ src/model/SystemConfigs.php
@@ -1,11 +1,40 @@
 <?php
 namespace xjryanse\system\model;
 
+use xjryanse\system\service\SystemFileService;
 /**
  * 配置表
  */
 class SystemConfigs extends Base
 {
-    
-
+    /**
+     * 值获取器，根据图片类型来区分
+     */
+    public function getValueAttr( $value, $data)
+    {
+        //上传图片
+        if($data['type'] == FR_COL_TYPE_UPLIMAGE){
+            return $value ? SystemFileService::getInstance( $value )->get() : $value ;
+        }
+        return $value;
+    }
+    /**
+     * 值修改器，
+     * @param type $value
+     * @param type $data
+     */
+    public function setValueAttr( $value, $data )
+    {
+        //获取数据类型
+        $type = "";
+        if($data['id']){
+            $info = self::get($data['id']);
+            $type = $info ? $info['type'] : '';
+        }
+        //上传图片只取id
+        if($type == FR_COL_TYPE_UPLIMAGE){
+            return is_array($value) ? $value['id'] : $value ;
+        }
+        return $value;
+    }
 }
