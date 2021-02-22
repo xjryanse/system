0b474548faab71a7eb1a2b300db67617badddb16
xieJialun <36151536+xjryanse@users.noreply.github.com>
2021-02-22 15:04:15 +0800

    1

--- src/model/SystemColumnList.php
+++ src/model/SystemColumnList.php
@@ -6,7 +6,59 @@ namespace xjryanse\system\model;
  */
 class SystemColumnList extends Base
 {
+    public function setIsListAttr( $value )
+    {
+        //非布尔值时转布尔值
+        return is_numeric($value) ? $value : booleanToNumber($value);
+    }
+
+    public function setIsListEditAttr( $value )
+    {
+        //非布尔值时转布尔值
+        return is_numeric($value) ? $value : booleanToNumber($value);
+    }
+    public function setIsAddAttr( $value )
+    {
+        //非布尔值时转布尔值
+        return is_numeric($value) ? $value : booleanToNumber($value);
+    }
+
+    public function setIsEditAttr( $value )
+    {
+        //非布尔值时转布尔值
+        return is_numeric($value) ? $value : booleanToNumber($value);
+    }    
+    public function setOnlyDetailAttr( $value )
+    {
+        //非布尔值时转布尔值
+        return is_numeric($value) ? $value : booleanToNumber($value);
+    }
     
+    public function setIsMustAttr( $value )
+    {
+        //非布尔值时转布尔值
+        return is_numeric($value) ? $value : booleanToNumber($value);
+    }    
+    public function setIsExportAttr( $value )
+    {
+        //非布尔值时转布尔值
+        return is_numeric($value) ? $value : booleanToNumber($value);
+    }
+    public function setIsImportMustAttr( $value )
+    {
+        //非布尔值时转布尔值
+        return is_numeric($value) ? $value : booleanToNumber($value);
+    }
+    public function setIsSpanCompanyAttr( $value )
+    {
+        //非布尔值时转布尔值
+        return is_numeric($value) ? $value : booleanToNumber($value);
+    }
+    public function setIsLinkageAttr( $value )
+    {
+        //非布尔值时转布尔值
+        return is_numeric($value) ? $value : booleanToNumber($value);
+    }    
     /**
      * 关联外键
      * @param type $value
